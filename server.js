4d02be6605100c6666681b0331cbfce777c99c13
unknown <tyler.igwe@gmail.com>
2024-03-20 17:04:09 +0100

    20/03/2024

--- server.js
+++ server.js
@@ -12,6 +12,16 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://book-store-five-red.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
 
 //routes
 
